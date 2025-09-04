49b44db679e1ac0497c1861b718662c651532512
guilhermebertogo10 <guilhermebertogo123@gmail.com>
2025-09-04 17:38:15 -0300

    ajuste

--- prisma/migrations/20250904203657_init/migration.sql
+++ prisma/migrations/20250904203657_init/migration.sql
@@ -1,41 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Cart` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Ingrediente` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Payment` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Product` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `habits` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropIndex
-DROP INDEX "habits_payment_id_key";
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Cart";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Ingrediente";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Payment";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Product";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "habits";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "orders" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -77,6 +39,18 @@ CREATE TABLE "products" (
 );
 
 -- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "CPF" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "carts" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "quantity" INTEGER NOT NULL,
@@ -87,21 +61,7 @@ CREATE TABLE "carts" (
 );
 
 -- CreateTable
-CREATE TABLE "ingredientes" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "description" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "quantify" TEXT NOT NULL,
-    "stock" TEXT NOT NULL,
-    "maturity" DATETIME NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL
-);
-
--- RedefineTables
-PRAGMA defer_foreign_keys=ON;
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_Receita" (
+CREATE TABLE "Receita" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "receita_id" INTEGER NOT NULL,
     "receitas_id" INTEGER NOT NULL,
@@ -115,11 +75,18 @@ CREATE TABLE "new_Receita" (
     CONSTRAINT "Receita_receita_id_fkey" FOREIGN KEY ("receita_id") REFERENCES "ingredientes" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "Receita_receitas_id_fkey" FOREIGN KEY ("receitas_id") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
-INSERT INTO "new_Receita" ("created_at", "description", "id", "maturity", "name", "quantify", "receita_id", "receitas_id", "stock", "updated_at") SELECT "created_at", "description", "id", "maturity", "name", "quantify", "receita_id", "receitas_id", "stock", "updated_at" FROM "Receita";
-DROP TABLE "Receita";
-ALTER TABLE "new_Receita" RENAME TO "Receita";
-PRAGMA foreign_keys=ON;
-PRAGMA defer_foreign_keys=OFF;
+
+-- CreateTable
+CREATE TABLE "ingredientes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "description" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantify" TEXT NOT NULL,
+    "stock" TEXT NOT NULL,
+    "maturity" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "orders_payment_id_key" ON "orders"("payment_id");
