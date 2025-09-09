df440cd338488aa0dd08623c090021f7c8743124
Ry4nDi4s <RyanDias52@outlook.com>
2025-09-09 16:55:44 -0300

    Atualização do Banco de Dados

--- README.md
+++ README.md
@@ -9,5 +9,3 @@ npx prisma studio
 npm run generate 
 
 npm run dev
-
-// cors seria uma liberação de quem pode chamar a variavel, por hora todo mundo pode chamar até que eu coloque alguma propriedade que possa chamar
---
+++ prisma/migrations/20250909194538_confeitaria/migration.sql
@@ -0,0 +1,92 @@
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "which_product" TEXT NOT NULL,
+    "who_order" TEXT NOT NULL,
+    "value" REAL NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "delivery_day" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "payment_id" INTEGER NOT NULL,
+    CONSTRAINT "orders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "orders_payment_id_fkey" FOREIGN KEY ("payment_id") REFERENCES "payments" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "card" TEXT NOT NULL,
+    "pix" TEXT NOT NULL,
+    "money" REAL NOT NULL,
+    "value" REAL NOT NULL,
+    "scheduling" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "description" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantify" INTEGER NOT NULL,
+    "stock" REAL NOT NULL,
+    "maturity" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "CPF" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "carts" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "quantity" INTEGER NOT NULL,
+    "produto_id" INTEGER NOT NULL,
+    "order_id" INTEGER NOT NULL,
+    CONSTRAINT "carts_produto_id_fkey" FOREIGN KEY ("produto_id") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "carts_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Receitas" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "receita_id" INTEGER NOT NULL,
+    "receitas_id" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantify" TEXT NOT NULL,
+    "stock" TEXT NOT NULL,
+    "maturity" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "Receitas_receita_id_fkey" FOREIGN KEY ("receita_id") REFERENCES "ingredientes" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Receitas_receitas_id_fkey" FOREIGN KEY ("receitas_id") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ingredientes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "description" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantify" TEXT NOT NULL,
+    "stock" TEXT NOT NULL,
+    "maturity" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orders_payment_id_key" ON "orders"("payment_id");
