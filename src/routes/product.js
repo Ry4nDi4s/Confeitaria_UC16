c54ebb64336a30937436b10ff011bb638a0a5e15
guilhermebertogo10 <guilhermebertogo123@gmail.com>
2025-09-18 15:19:02 -0300

    put esta feito

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,6 +7,7 @@ route.post('/', ProductController.store);
 route.get('/', ProductController.index);
 route.get('/:id', ProductController.show);
 route.delete('/:id', ProductController.delete);
-route.put('/:id', ProductController.put);
+route.put('/:id', ProductController.update);
+
 
 export default route ; 
