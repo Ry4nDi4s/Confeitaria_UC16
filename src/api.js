8886eacc1e630aed4962b243311b89d71866de0a
Ry4nDi4s <RyanDias52@outlook.com>
2026-04-01 14:50:49 -0300

    feat(prisma) adição do PRISMA 7 Version

--- src/api.js
+++ src/api.js
@@ -19,7 +19,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const app = express();
-app.use(cors());
 app.use(express.json());
 app.use("/static", express.static(path.join(__dirname, "../static")));
 
